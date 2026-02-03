b2b62823b807d4bae63f221a137511fe60718a1d
JAVOHIRQAYUMOV14050725 <qayumovjavohir2@gmail.com>
2026-02-03 18:04:13 +0500

    fixed

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -1,27 +1,12 @@
 import { Module } from '@nestjs/common';
-import { EscrowService } from './escrow.service';
-import { LedgerService } from './ledger.service';
-import { WalletService } from './wallet.service';
-import { PaymentsService } from './payments.service';
-import { OpsModule } from '@/modules/ops/ops.module';
 import { PaymentsController } from './payments.controller';
 import { AuthModule } from '../auth/auth.module';
+import { PaymentsCoreModule } from './payments-core.module';
 
 
 @Module({
-    imports: [OpsModule, AuthModule],
+    imports: [PaymentsCoreModule, AuthModule],
     controllers: [PaymentsController],
-    providers: [
-        EscrowService,
-        LedgerService,
-        WalletService,
-        PaymentsService,
-    ],
-    exports: [
-        EscrowService,
-        LedgerService,
-        WalletService,
-        PaymentsService,
-    ],
+    exports: [PaymentsCoreModule],
 })
 export class PaymentsModule { }
